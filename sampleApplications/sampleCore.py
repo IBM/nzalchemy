d000ac2cce132bb9ced797eb848945766916d8a6
Ayush Mehrotra <ayush@Ayushs-MacBook-Pro.local>
2024-10-30 17:38:24 +0530

    updated the test scripts
    Signed-off-by: Ayush Mehrotra <ayush@Ayushs-MacBook-Pro.local>
    Signed-off-by: Ayush Mehrotra <ayush.mehrotra900@gmail.com>

--- sampleApplications/sampleCore.py
+++ sampleApplications/sampleCore.py
@@ -1,15 +1,23 @@
-import pyodbc
-import urllib
+import os
+import sys
+import nzpy
 from sqlalchemy import create_engine, MetaData, Table, Integer, String, Column, DateTime, text, asc, desc, alias, text, between, func
 from datetime import datetime
 from sqlalchemy.types import CHAR
 from sqlalchemy.types import VARCHAR
 from sqlalchemy import select, insert
 
-metadata = MetaData()
+print ("\n--------- " + sys.argv[0] + " ---------\n")
 
-params = urllib.parse.quote_plus("DRIVER=NetezzaSQL;SERVER=longpassword1.fyre.ibm.com;PORT=5480;DATABASE=TESTODBC;UID=admin;PWD=password")
-engine = create_engine("nz+pyodbc:///?odbc_connect=%s" % params, echo=True)
+host = os.getenv("MY_HOST")
+user = os.getenv("MY_USER")
+password = os.getenv("MY_PASSWORD")
+db = os.getenv("MY_DB")
+port = os.getenv("MY_PORT")
+
+def creator():
+    return nzpy.connect(user=f"{user}", password=f"{password}",host=f"{host}", port=int(port), database=f"{db}", securityLevel=0,logOptions=nzpy.LogOptions.Logfile, char_varchar_encoding='utf8')
+engine = create_engine("netezza+nzpy://", creator=creator) 
 
 meta = MetaData()
 employee = Table(
@@ -20,11 +28,11 @@ employee = Table(
 )
 meta.create_all(engine)
 
-print(employee.columns)         # return a list of columns
-print(employee.c)               # same as posts.columns
-print(employee.foreign_keys)    # returns a set containing foreign keys on the table
-print(employee.primary_key)     # returns the primary key of the table
-print(employee.metadata)        # get the MetaData object from the table
+print(employee.columns)
+print(employee.c)
+print(employee.foreign_keys)
+print(employee.primary_key)
+print(employee.metadata)
 
 #conn for insert and select
 conn = engine.connect()
@@ -47,11 +55,11 @@ conn.execute(employee.insert(), [
 ])
 
 #func
-result = conn.execute(select([func.max(employee.c.id)]))
+result = conn.execute(select(func.max(employee.c.id)))
 print (result.fetchone())
 
 #select
-s = select([employee])  
+s = select(employee)  
 result = conn.execute(s)
 for row in result:
     print (row)
@@ -63,24 +71,24 @@ for row in result:
     print (row)
 
 # like statement
-s = select([employee]).where(employee.c.name.like("ja%"))
+s = select(employee).where(employee.c.name.like("ja%"))
 result = conn.execute(s)
 for row in result:
     print (row)
 
 #select using limit
-s = select([employee]).limit(2)  
+s = select(employee).limit(2)  
 result = conn.execute(s)
 for row in result:
     print (row)
 
 # order by    
-s = select([employee]).order_by(desc(employee.c.id)) 
+s = select(employee).order_by(desc(employee.c.id)) 
 print(conn.execute(s).fetchall())
    
 #alias   
 st = employee.alias("a")
-s = select([st]).where(st.c.id > 2)
+s = select(st).where(st.c.id > 2)
 print(conn.execute(s).fetchall())
     
 #using text firing query    
@@ -89,16 +97,16 @@ print(conn.execute(t).fetchall())
 
 #using text and binding params
 s = text("select id, gender from employee where id between :x and :y")
-print(conn.execute(s, x = 10, y = 22).fetchall())
+print(conn.execute(s, {"x": 10, "y": 22}).fetchall())
 
 # using between
-stmt = select([employee]).where(between(employee.c.id,20,40))
+stmt = select(employee).where(between(employee.c.id,20,40))
 print (conn.execute(stmt).fetchall())
 
 #update
 updt = employee.update().where(employee.c.id == '13').values(name='changed')
 result = conn.execute(updt)
-s = select([employee])
+s = select(employee)
 result = conn.execute(s)
 for row in result:
     print (row)
@@ -106,7 +114,7 @@ for row in result:
 #Delete Row/s
 delt = employee.delete().where(employee.c.name == 'changed')
 conn.execute(delt)
-s = select([employee])
+s = select(employee)
 result = conn.execute(s)
 for row in result:
     print (row)    
